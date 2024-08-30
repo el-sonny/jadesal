d05f26eb14353a9b086017f99f16ecab78033fa1
Salvador Barron <sonny@spaceshiplabs.com>
2024-08-30 12:32:12 -0500

    add philosopher font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				serif: ['Cormorant Garamond']
+				sans: ['Philosopher', ...defaultTheme.fontFamily.sans],
+				serif: ['Cormorant Garamond', ...defaultTheme.fontFamily.serif]
 			}
 		}
 	},
